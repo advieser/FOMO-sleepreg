3b1b345a8df26b8306b863e2512b246423dee8ac
kenomersmannPC <advieser@gmail.com>
2024-03-19 23:06:17 +0100

    fix1 post-submission

--- R/implementation.R
+++ R/implementation.R
@@ -4,8 +4,9 @@
 # t: clock time in decimal hours since start of experiment
 # Fix for all agents; min and max as specified in paper
 process_c <- function(t) {
-  -0.5 * (sin((t + 2) * pi/12) - 1)
+  0.5 * (sin((t + 2) * pi/12) - 1)
 }
+# removing minus sign might fix some things
 
 # Process S
 # t: clock time in decimal hours since start of experiment
@@ -46,7 +47,7 @@ agent_start <- list(start_state = 1, # is awake in beginning
                     bedtime = 23)
 
 # set up time steps in hours, starting with beginning of experiment
-time_steps <- seq(7, 72, by = 1) # starting with 7:00 on travel day
+time_steps <- seq(7, 72, by = 0.1) # starting with 7:00 on travel day
 
 # for each time point:
 # - calculate current sleep propensity
@@ -102,4 +103,6 @@ simulate_trial <- function(agent, time_steps) {
 }
 
 # --- Simulation ---
-simulate_trial(agent = agent_start, time_steps = time_steps)
+trial <- simulate_trial(agent = agent_start, time_steps = time_steps)
+plot(time_steps, trial$sleep_propensity[-nrow(trial)])
+plot(time_steps, trial$states[-nrow(trial)])
